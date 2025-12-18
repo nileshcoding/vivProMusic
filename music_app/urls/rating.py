0f453e078aabd590d3e395e7c2cc7e3b8c2299f5
nileshcoding <nileshkumar98@outlook.com>
2025-12-18 09:26:36 +0530

    separated rating url file

--- music_app/urls/rating.py
+++ music_app/urls/rating.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from ..views.music import MusicViewSet
+from ..views.rating import RatingViewSet
 
 router = DefaultRouter()
 
-router.register(r'music', MusicViewSet, basename='music')
+router.register(r'rating', RatingViewSet, basename='rating')
 
 urlpatterns = [
     path('', include(router.urls)),
